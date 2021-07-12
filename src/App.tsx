515bc7070301eea6d95ee270e7ba8575e44c51aa
Neil Brian Labayna <neilbrian.nl@estl.sg>
2021-07-13 00:45:33 +0800

    Group stations

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,27 @@ import { ReactElement, useState } from 'react';
 import './styles/App.scss';
 import MainDisplay from './components/MainDisplay/index';
 import StationSelector from './components/StationSelector';
+import grouped from './data/grouped';
+// import stations from './data/trainStations';
 
+// interface Station {
+//   code: string;
+//   name: string;
+// }
 
 const App = (): ReactElement => {
+  // const stationsMap: { [key: string]: Array<Station> } = {};
+  // stations.forEach(s => {
+  //   console.log(s);
+  //   const station = { code: s[0], name: s[1] };
+  //   if (!stationsMap[s[3]]) {
+  //     stationsMap[s[3]] = [station];
+  //   } else {
+  //     stationsMap[s[3]].push(station);
+  //   }
+  // });
+  console.log(grouped);
+
   const [startStation, setStartStation] = useState('');
   const [endStation, setEndStation] = useState('');
   // true is heading to JE
---
+++ src/data/grouped.ts
@@ -0,0 +1,778 @@
+/* eslint-disable */
+const grouped = {
+  'North South Line': [
+    {
+      code: 'NS1',
+      name: 'Jurong East'
+    },
+    {
+      code: 'NS2',
+      name: 'Bukit Batok'
+    },
+    {
+      code: 'NS3',
+      name: 'Bukit Gombak'
+    },
+    {
+      code: 'NS4',
+      name: 'Choa Chu Kang'
+    },
+    {
+      code: 'NS5',
+      name: 'Yew Tee'
+    },
+    {
+      code: 'NS7',
+      name: 'Kranji'
+    },
+    {
+      code: 'NS8',
+      name: 'Marsiling'
+    },
+    {
+      code: 'NS9',
+      name: 'Woodlands'
+    },
+    {
+      code: 'NS10',
+      name: 'Admiralty'
+    },
+    {
+      code: 'NS11',
+      name: 'Sembawang'
+    },
+    {
+      code: 'NS12',
+      name: 'Canberra'
+    },
+    {
+      code: 'NS13',
+      name: 'Yishun'
+    },
+    {
+      code: 'NS14',
+      name: 'Khatib'
+    },
+    {
+      code: 'NS15',
+      name: 'Yio Chu Kang'
+    },
+    {
+      code: 'NS16',
+      name: 'Ang Mo Kio'
+    },
+    {
+      code: 'NS17',
+      name: 'Bishan'
+    },
+    {
+      code: 'NS18',
+      name: 'Braddell'
+    },
+    {
+      code: 'NS19',
+      name: 'Toa Payoh'
+    },
+    {
+      code: 'NS20',
+      name: 'Novena'
+    },
+    {
+      code: 'NS21',
+      name: 'Newton'
+    },
+    {
+      code: 'NS22',
+      name: 'Orchard'
+    },
+    {
+      code: 'NS23',
+      name: 'Somerset'
+    },
+    {
+      code: 'NS24',
+      name: 'Dhoby Ghaut'
+    },
+    {
+      code: 'NS25',
+      name: 'City Hall'
+    },
+    {
+      code: 'NS26',
+      name: 'Raffles Place'
+    },
+    {
+      code: 'NS27',
+      name: 'Marina Bay'
+    },
+    {
+      code: 'NS28',
+      name: 'Marina South Pier'
+    }
+  ],
+  'East West Line': [
+    {
+      code: 'EW1',
+      name: 'Pasir Ris'
+    },
+    {
+      code: 'EW2',
+      name: 'Tampines'
+    },
+    {
+      code: 'EW3',
+      name: 'Simei'
+    },
+    {
+      code: 'EW4',
+      name: 'Tanah Merah'
+    },
+    {
+      code: 'EW5',
+      name: 'Bedok'
+    },
+    {
+      code: 'EW6',
+      name: 'Kembangan'
+    },
+    {
+      code: 'EW7',
+      name: 'Eunos'
+    },
+    {
+      code: 'EW8',
+      name: 'Paya Lebar'
+    },
+    {
+      code: 'EW9',
+      name: 'Aljunied'
+    },
+    {
+      code: 'EW10',
+      name: 'Kallang'
+    },
+    {
+      code: 'EW11',
+      name: 'Lavender'
+    },
+    {
+      code: 'EW12',
+      name: 'Bugis'
+    },
+    {
+      code: 'EW13',
+      name: 'City Hall'
+    },
+    {
+      code: 'EW14',
+      name: 'Raffles Place'
+    },
+    {
+      code: 'EW15',
+      name: 'Tanjong Pagar'
+    },
+    {
+      code: 'EW16',
+      name: 'Outram Park'
+    },
+    {
+      code: 'EW17',
+      name: 'Tiong Bahru'
+    },
+    {
+      code: 'EW18',
+      name: 'Redhill'
+    },
+    {
+      code: 'EW19',
+      name: 'Queenstown'
+    },
+    {
+      code: 'EW20',
+      name: 'Commonwealth'
+    },
+    {
+      code: 'EW21',
+      name: 'Buona Vista'
+    },
+    {
+      code: 'EW22',
+      name: 'Dover'
+    },
+    {
+      code: 'EW23',
+      name: 'Clementi'
+    },
+    {
+      code: 'EW24',
+      name: 'Jurong East'
+    },
+    {
+      code: 'EW25',
+      name: 'Chinese Garden'
+    },
+    {
+      code: 'EW26',
+      name: 'Lakeside'
+    },
+    {
+      code: 'EW27',
+      name: 'Boon Lay'
+    },
+    {
+      code: 'EW28',
+      name: 'Pioneer'
+    },
+    {
+      code: 'EW29',
+      name: 'Joo Koon'
+    },
+    {
+      code: 'EW30',
+      name: 'Gul Circle'
+    },
+    {
+      code: 'EW31',
+      name: 'Tuas Crescent'
+    },
+    {
+      code: 'EW32',
+      name: 'Tuas West Road'
+    },
+    {
+      code: 'EW33',
+      name: 'Tuas Link'
+    }
+  ],
+  'Changi Airport Branch Line': [
+    {
+      code: 'CG1',
+      name: 'Expo'
+    },
+    {
+      code: 'CG2',
+      name: 'Changi Airport'
+    }
+  ],
+  'North East Line': [
+    {
+      code: 'NE1',
+      name: 'HarbourFront'
+    },
+    {
+      code: 'NE3',
+      name: 'Outram Park'
+    },
+    {
+      code: 'NE4',
+      name: 'Chinatown'
+    },
+    {
+      code: 'NE5',
+      name: 'Clarke Quay'
+    },
+    {
+      code: 'NE6',
+      name: 'Dhoby Ghaut'
+    },
+    {
+      code: 'NE7',
+      name: 'Little India'
+    },
+    {
+      code: 'NE8',
+      name: 'Farrer Park'
+    },
+    {
+      code: 'NE9',
+      name: 'Boon Keng'
+    },
+    {
+      code: 'NE10',
+      name: 'Potong Pasir'
+    },
+    {
+      code: 'NE11',
+      name: 'Woodleigh'
+    },
+    {
+      code: 'NE12',
+      name: 'Serangoon'
+    },
+    {
+      code: 'NE13',
+      name: 'Kovan'
+    },
+    {
+      code: 'NE14',
+      name: 'Hougang'
+    },
+    {
+      code: 'NE15',
+      name: 'Buangkok'
+    },
+    {
+      code: 'NE16',
+      name: 'Sengkang'
+    },
+    {
+      code: 'NE17',
+      name: 'Punggol'
+    }
+  ],
+  'Circle Line': [
+    {
+      code: 'CC1',
+      name: 'Dhoby Ghaut'
+    },
+    {
+      code: 'CC2',
+      name: 'Bras Basah'
+    },
+    {
+      code: 'CC3',
+      name: 'Esplanade'
+    },
+    {
+      code: 'CC4',
+      name: 'Promenade'
+    },
+    {
+      code: 'CC5',
+      name: 'Nicoll Highway'
+    },
+    {
+      code: 'CC6',
+      name: 'Stadium'
+    },
+    {
+      code: 'CC7',
+      name: 'Mountbatten'
+    },
+    {
+      code: 'CC8',
+      name: 'Dakota'
+    },
+    {
+      code: 'CC9',
+      name: 'Paya Lebar'
+    },
+    {
+      code: 'CC10',
+      name: 'MacPherson'
+    },
+    {
+      code: 'CC11',
+      name: 'Tai Seng'
+    },
+    {
+      code: 'CC12',
+      name: 'Bartley'
+    },
+    {
+      code: 'CC13',
+      name: 'Serangoon'
+    },
+    {
+      code: 'CC14',
+      name: 'Lorong Chuan'
+    },
+    {
+      code: 'CC15',
+      name: 'Bishan'
+    },
+    {
+      code: 'CC16',
+      name: 'Marymount'
+    },
+    {
+      code: 'CC17',
+      name: 'Caldecott'
+    },
+    {
+      code: 'CC19',
+      name: 'Botanic Gardens'
+    },
+    {
+      code: 'CC20',
+      name: 'Farrer Road'
+    },
+    {
+      code: 'CC21',
+      name: 'Holland Village'
+    },
+    {
+      code: 'CC22',
+      name: 'Buona Vista'
+    },
+    {
+      code: 'CC23',
+      name: 'one-north'
+    },
+    {
+      code: 'CC24',
+      name: 'Kent Ridge'
+    },
+    {
+      code: 'CC25',
+      name: 'Haw Par Villa'
+    },
+    {
+      code: 'CC26',
+      name: 'Pasir Panjang'
+    },
+    {
+      code: 'CC27',
+      name: 'Labrador Park'
+    },
+    {
+      code: 'CC28',
+      name: 'Telok Blangah'
+    },
+    {
+      code: 'CC29',
+      name: 'HarbourFront'
+    }
+  ],
+  'Circle Line Extension': [
+    {
+      code: 'CE1',
+      name: 'Bayfront'
+    },
+    {
+      code: 'CE2',
+      name: 'Marina Bay'
+    }
+  ],
+  'Downtown Line': [
+    {
+      code: 'DT1',
+      name: 'Bukit Panjang'
+    },
+    {
+      code: 'DT2',
+      name: 'Cashew'
+    },
+    {
+      code: 'DT3',
+      name: 'Hillview'
+    },
+    {
+      code: 'DT5',
+      name: 'Beauty World'
+    },
+    {
+      code: 'DT6',
+      name: 'King Albert Park'
+    },
+    {
+      code: 'DT7',
+      name: 'Sixth Avenue'
+    },
+    {
+      code: 'DT8',
+      name: 'Tan Kah Kee'
+    },
+    {
+      code: 'DT9',
+      name: 'Botanic Gardens'
+    },
+    {
+      code: 'DT10',
+      name: 'Stevens'
+    },
+    {
+      code: 'DT11',
+      name: 'Newton'
+    },
+    {
+      code: 'DT12',
+      name: 'Little India'
+    },
+    {
+      code: 'DT13',
+      name: 'Rochor'
+    },
+    {
+      code: 'DT14',
+      name: 'Bugis'
+    },
+    {
+      code: 'DT15',
+      name: 'Promenade'
+    },
+    {
+      code: 'DT16',
+      name: 'Bayfront'
+    },
+    {
+      code: 'DT17',
+      name: 'Downtown'
+    },
+    {
+      code: 'DT18',
+      name: 'Telok Ayer'
+    },
+    {
+      code: 'DT19',
+      name: 'Chinatown'
+    },
+    {
+      code: 'DT20',
+      name: 'Fort Canning'
+    },
+    {
+      code: 'DT21',
+      name: 'Bencoolen'
+    },
+    {
+      code: 'DT22',
+      name: 'Jalan Besar'
+    },
+    {
+      code: 'DT23',
+      name: 'Bendemeer'
+    },
+    {
+      code: 'DT24',
+      name: 'Geylang Bahru'
+    },
+    {
+      code: 'DT25',
+      name: 'Mattar'
+    },
+    {
+      code: 'DT26',
+      name: 'MacPherson'
+    },
+    {
+      code: 'DT27',
+      name: 'Ubi'
+    },
+    {
+      code: 'DT28',
+      name: 'Kaki Bukit'
+    },
+    {
+      code: 'DT29',
+      name: 'Bedok North'
+    },
+    {
+      code: 'DT30',
+      name: 'Bedok Reservoir'
+    },
+    {
+      code: 'DT31',
+      name: 'Tampines West'
+    },
+    {
+      code: 'DT32',
+      name: 'Tampines'
+    },
+    {
+      code: 'DT33',
+      name: 'Tampines East'
+    },
+    {
+      code: 'DT34',
+      name: 'Upper Changi'
+    },
+    {
+      code: 'DT35',
+      name: 'Expo'
+    }
+  ],
+  'Bukit Panjang LRT': [
+    {
+      code: 'BP1',
+      name: 'Choa Chu Kang'
+    },
+    {
+      code: 'BP2',
+      name: 'South View'
+    },
+    {
+      code: 'BP3',
+      name: 'Keat Hong'
+    },
+    {
+      code: 'BP4',
+      name: 'Teck Whye'
+    },
+    {
+      code: 'BP5',
+      name: 'Phoenix'
+    },
+    {
+      code: 'BP6',
+      name: 'Bukit Panjang'
+    },
+    {
+      code: 'BP7',
+      name: 'Petir'
+    },
+    {
+      code: 'BP8',
+      name: 'Pending'
+    },
+    {
+      code: 'BP9',
+      name: 'Bangkit'
+    },
+    {
+      code: 'BP10',
+      name: 'Fajar'
+    },
+    {
+      code: 'BP11',
+      name: 'Segar'
+    },
+    {
+      code: 'BP12',
+      name: 'Jelapang'
+    },
+    {
+      code: 'BP13',
+      name: 'Senja'
+    },
+    {
+      code: 'BP14',
+      name: 'Ten Mile Junction'
+    }
+  ],
+  'Sengkang LRT': [
+    {
+      code: 'STC',
+      name: 'Sengkang'
+    },
+    {
+      code: 'SE1',
+      name: 'Compassvale'
+    },
+    {
+      code: 'SE2',
+      name: 'Rumbia'
+    },
+    {
+      code: 'SE3',
+      name: 'Bakau'
+    },
+    {
+      code: 'SE4',
+      name: 'Kangkar'
+    },
+    {
+      code: 'SE5',
+      name: 'Ranggung'
+    },
+    {
+      code: 'SW1',
+      name: 'Cheng Lim'
+    },
+    {
+      code: 'SW2',
+      name: 'Farmway'
+    },
+    {
+      code: 'SW3',
+      name: 'Kupang'
+    },
+    {
+      code: 'SW4',
+      name: 'Thanggam'
+    },
+    {
+      code: 'SW5',
+      name: 'Fernvale'
+    },
+    {
+      code: 'SW6',
+      name: 'Layar'
+    },
+    {
+      code: 'SW7',
+      name: 'Tongkang'
+    },
+    {
+      code: 'SW8',
+      name: 'Renjong'
+    }
+  ],
+  'Punggol LRT': [
+    {
+      code: 'PTC',
+      name: 'Punggol'
+    },
+    {
+      code: 'PE1',
+      name: 'Cove'
+    },
+    {
+      code: 'PE2',
+      name: 'Meridian'
+    },
+    {
+      code: 'PE3',
+      name: 'Coral Edge'
+    },
+    {
+      code: 'PE4',
+      name: 'Riviera'
+    },
+    {
+      code: 'PE5',
+      name: 'Kadaloor'
+    },
+    {
+      code: 'PE6',
+      name: 'Oasis'
+    },
+    {
+      code: 'PE7',
+      name: 'Damai'
+    },
+    {
+      code: 'PW1',
+      name: 'Sam Kee'
+    },
+    {
+      code: 'PW2',
+      name: 'Teck Lee'
+    },
+    {
+      code: 'PW3',
+      name: 'Punggol Point'
+    },
+    {
+      code: 'PW4',
+      name: 'Samudera'
+    },
+    {
+      code: 'PW5',
+      name: 'Nibong'
+    },
+    {
+      code: 'PW6',
+      name: 'Sumang'
+    },
+    {
+      code: 'PW7',
+      name: 'Soo Teck'
+    }
+  ],
+  'Thomson-East Coast Line': [
+    {
+      code: 'TE1',
+      name: 'Woodlands North '
+    },
+    {
+      code: 'TE2',
+      name: 'Woodlands '
+    },
+    {
+      code: 'TE3',
+      name: 'Woodlands South '
+    }
+  ]
+};
+export default grouped;
